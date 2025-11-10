a7287529caf3688a7acd7d43b68a08be38c460ae
jerriker <befikerkassahuntessema@gmail.com>
2025-11-11 00:25:06 +0300

    remove css reset

---
+++ check.js
@@ -0,0 +1,94 @@
+sketch = document.querySelector('.sketch')
+cellSizeDisplay = document.querySelector('.size-display')
+inputSize = document.getElementById('size')
+
+// Buttons
+randomColorBtn = document.getElementById('random-color')
+clearBtn = document.getElementById('clear')
+gridBtn = document.getElementById('grid')
+
+let currentMode = 'default'
+
+// Default size
+const defaultSize = 16
+
+cellSizeDisplay.textContent = `${defaultSize}X${defaultSize}`
+inputSize.value = defaultSize
+
+for (let i = 1; i <= defaultSize ** 2; i++) {
+    const defaults = document.createElement('div')
+    defaults.classList.add('cell')
+    defaults.style.width = `${100 / defaultSize}%`
+    defaults.style.height = `${100 / defaultSize}%`
+
+    defaults.addEventListener('mouseover', () => {
+        if (userRandomColor)
+            defaults.style.backgroundColor = randomColorGenerate()
+        // else (useClearBtn)
+        // defaults.style.backgroundColor = 'red'
+
+    })
+    sketch.appendChild(defaults)
+}
+
+
+
+// Random Color
+const randomColorGenerate = () => {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+let userRandomColor = false
+randomColorBtn.addEventListener('click', () => {
+    userRandomColor = !userRandomColor
+    randomColorBtn.style.backgroundColor = 'green'
+    currentMode = 'randomColor'
+})
+
+let useClearBtn = false
+clearBtn.addEventListener('click', () => {
+    useClearBtn = !useClearBtn
+    currentMode = 'clear'
+})
+
+// Modify Size 
+
+const createCell = () => {
+    cellSizeDisplay.textContent = `${inputSize.value}X${inputSize.value}`
+    sketch.innerHTML = ''
+
+    let size = inputSize.value
+    for (let i = 1; i <= size ** 2; i++) {
+        const newCell = document.createElement('div')
+        newCell.classList.add('cell')
+        newCell.style.width = `${100 / size}%`
+        newCell.style.height = `${100 / size}%`
+
+        newCell.addEventListener('mouseover', () => {
+
+            ;
+        })
+        sketch.appendChild(newCell)
+
+    }
+}
+
+inputSize.addEventListener('input', () => createCell())
+
+// Grid
+let gridEnabled = true;
+
+gridBtn.addEventListener('click', () => {
+    const cells = document.querySelectorAll('.cell');
+    gridEnabled = !gridEnabled;
+
+    cells.forEach(cell => {
+
+        cell.style.border = gridEnabled ? '1px solid #ddd' : 'none';
+    });
+
+    // gridBtn.textContent = gridEnabled ? 'Grid ON' : 'Grid OFF';
+});
